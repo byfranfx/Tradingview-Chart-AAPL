38e6eadc9d125d43b8b0c3dc3bd7c7e70e5981ff
byfranfx <71705230+byfranfx@users.noreply.github.com>
2020-09-22 15:00:53 +0200

    update dark btc list

--- popup/widget.js
+++ popup/widget.js
@@ -1,17 +1,24 @@
 // The Tradingview widget that is to be displayed
 new TradingView.widget(
     {
-        "width": 780,
+        "width": 1250,
         "height": 570,
-        "symbol": "NASDAQ:AAPL",
-        "interval": "D",
+        "symbol": "BINANCE:BTCUSDTPERP",
+        "interval": "60",
         "timezone": "Etc/UTC",
-        "theme": "Light",
+        "theme": "dark",
         "style": "1",
         "locale": "en",
         "toolbar_bg": "#f1f3f6",
         "enable_publishing": false,
         "allow_symbol_change": true,
-        "container_id": "tradingview_0fe5a"
+	"watchlist": [
+		"BINANCE:BTCUSDTPERP",
+		"BINANCE:ETHUSDTPERP",
+		"BINANCE:DOTUSDTPERP",
+		"BINANCE:ADASDTPERP",
+		"BINANCE:TRXUSDTPERP",
+	],
+        "container_id": "tradingview_912d9"
     }
 );
